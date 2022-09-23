d55c976269704bde4e68c50f405c8615d070e196
Ian_S <iansalomao17@gmail.com>
2022-09-23 00:34:44 -0300

    Front end tela de menu gerencial
    Front end da tela de menu gerencial, também adicionei os modais de exclusão e edição de itens.

---
+++ modals/_js/script.js
@@ -0,0 +1,57 @@
+const inputFile = document.querySelector("#picture-input");
+const pictureImage = document.querySelector(".picture-image");
+const pictureImageTxt = "Escolha uma imagem";
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
+
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture-img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
+});
+
+// Modals
+
+const bg_modal = document.querySelector(".modals ");
+
+// Add
+var btn_add = document.querySelector("#btn-add");
+const modal_add = document.querySelector(".modal-add");
+
+btn_add.addEventListener("click", () => {
+  bg_modal.style.display = "flex";
+  modal_add.style.display = "flex";
+});
+
+//Delet
+var btn_delet = document.querySelector(".edit-delet>span");
+const modal_delet = document.querySelector(".modal-delet");
+btn_delet.addEventListener("click", () => {
+  bg_modal.style.display = "flex";
+  modal_delet.style.display = "flex";
+});
+
+var btn_cancel = document.querySelector("#btn-cancel");
+
+btn_cancel.addEventListener("click", () => {
+  bg_modal.style.display = "none";
+  modal_add.style.display = "none";
+  modal_delet.style.display = "none";
+});
---
+++ modals/modal_add.php
@@ -0,0 +1,31 @@
+<div class="modal-add">
+    <form action="" class="form-add">
+    <h2>Adicionar produto</h2>
+    <div>
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Valor" />
+    </div>
+    <div>
+        <select name="select">
+        <option value="valor1">Valor 1</option>
+        <option value="valor2">Valor 2</option>
+        <option value="valor3">Valor 3</option>
+        </select>
+        <select name="select">
+        <option value="valor1">Valor 1</option>
+        <option value="valor2">Valor 2</option>
+        <option value="valor3">Valor 3</option>
+        </select>
+    </div>
+
+    <div>
+        <label class="picture" for="picture-input">
+        <span class="picture-image"></span>
+        </label>
+        <input type="file" name="picture-input" id="picture-input" />
+        <textarea maxlength="500" placeholder="Descrição"></textarea>
+    </div>
+    <button class="btn" >Salvar item</button>
+    <button class="btn" >cancelar</button>
+    </form>
+</div>
