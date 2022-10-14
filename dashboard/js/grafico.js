b17173761a8292cc8cc26a8449f86c0610b46b8b
Ian_S <iansalomao17@gmail.com>
2022-10-14 14:06:17 -0300

    Alterações dashboard_Esdras

--- dashboard/js/grafico.js
+++ dashboard/js/grafico.js
@@ -1,37 +1,42 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ]);
 
-    var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-    ]);
-
-    var options = {
-    title: 'My Daily Activities',
-    'legend':'bottom',
-    width:800,
-    height:400,
-    responsive: true
+  var options = {
+    // title: "My Daily Activities",
+    legend: "bottom",
+    width: 800,
+    height: 400,
+    responsive: true,
     // backgroundColor: {
-    //     fill:'gray'     
+    //     fill:'gray'
     //     }
-    };
+  };
 
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    
-    var barchart_options = {title:'Barchart: How Much Pizza I Ate Last Night',
-                       width:800,
-                       height:400,
-                       responsive: true,
-                       legend: 'none'};
-    var barchart = new google.visualization.BarChart(document.getElementById('barchart'));
-    barchart.draw(data, barchart_options);
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
 
-    chart.draw(data, options);
+  var barchart_options = {
+    // title: "Barchart: How Much Pizza I Ate Last Night",
+    width: 800,
+    height: 400,
+    responsive: true,
+    legend: "none",
+  };
+  var barchart = new google.visualization.BarChart(
+    document.getElementById("barchart")
+  );
+  barchart.draw(data, barchart_options);
+
+  chart.draw(data, options);
 }
