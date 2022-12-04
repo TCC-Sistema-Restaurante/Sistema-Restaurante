159b60f6921c4e790cb144c8e787c6ab38ab0b5e
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2022-12-04 15:05:43 -0300

    Revisão nav_bar.html para nav_bar.php

--- nav_bar/nav.php
+++ nav_bar/nav.php
@@ -1,15 +1,16 @@
-<link rel="stylesheet" href="_CSS/nav.css" />
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
 />
+<link rel="stylesheet" href="_CSS/nav.css">
+
 
 <div class="topnav" id="myTopnav">
   <a href="#" class="active">Voltar</a>
 
   <a href="#">Pedidos</a>
   <a href="#">Cardápio</a>
-  <div class="dropdown">
+  <div class="dropdownMenu">
     <button class="dropbtn">
       Cadastros<span class="material-symbols-outlined"> arrow_drop_down </span>
     </button>
@@ -20,14 +21,8 @@
     </div>
   </div>
   <a href="#">Dashboard</a>
-
-  <a
-    href="javascript:void(0);"
-    style="font-size: 15px"
-    class="icon"
-    onclick="myFunction()"
-    >&#9776;</a
-  >
+  <a href="javascript:void(0);" style="font-size: 15px" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
+
 
 <script src="_JS/script.js"></script>
