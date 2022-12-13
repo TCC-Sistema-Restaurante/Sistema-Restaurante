c7996c3ba9836481796d2df41b0bbb04361d55fb
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2022-12-13 20:41:09 -0300

    Revisão Back-end cadastro_usuario
    1- Remoção da função da navbar (função já está incluída no include)
    1- Remoção do arquivo nav.css

---
+++ _scripts/config_pdo.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO('mysql:host=sql541.main-hosting.eu; dbname=u668629163_gusteau_databa;','u668629163_gusteau','Gusteau@123');
+
--- cadastro_usuario/cadastro_usuario.php
+++ cadastro_usuario/cadastro_usuario.php
@@ -82,21 +82,10 @@
           $('.select').niceSelect();
         });
 
-
-        
-        function myFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-        x.className += " responsive";
-        } else {
-        x.className = "topnav";
-        }
-        }
     </script>
     
 </body>
 </html>
-
 
 <?php
 
