9d8aea3c06b2d386ce2ac89fca006fbd6cc88dd5
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2022-12-13 20:57:57 -0300

    Update de function.php
    1-  função de tratar_arquivo_upload para o upload

--- _scripts/functions.php
+++ _scripts/functions.php
@@ -9,7 +9,7 @@ function login($dados){
     $senha = $mysqli->real_escape_string($dados['senha']);
 
     //Código sql para checar na tabela "dados_user" se o úsuario e a senha estão corretos
-    $sql = "SELECT * FROM dados_user WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT * FROM usuario WHERE usuario = '$usuario' AND senha = '$senha'";
     //Executa o código sql e em caso de falha retorna o erro
     $query = $mysqli->query($sql) or die("ERRO: " . $mysqli->error);
 
@@ -30,12 +30,30 @@ function login($dados){
         if($_SESSION['cargo'] == "Gerente"){
             header("Location: ../menu/menu.html");
         }
-        else if($_SESSION['cargo'] == "Cozinha"){
+        else if($_SESSION['cargo'] == "Cozinheiro"){
             header("Location: ../pedidos pendentes/index.html");
 
         }
-        else{
+        else if($_SESSION['cargo'] == "Garçom"){
             header("Location: ../cardapio_garcom/cardapio.html");
+
+        }
+        else{
+            ?>
+            <script language='javascript'>
+                swal.fire({
+                    icon: "error",
+                    text: "Usuário não possui cargo definido",
+                    type: "success",
+                    confirmButtonText: 'Tentar Novamente',
+                    timer: 7500,
+                    confirmButtonColor: '#34A323'
+    
+                }).then(okay => {
+    
+                });
+            </script>
+        <?php
         }
     } else {
     ?>
@@ -56,6 +74,8 @@ function login($dados){
     }
 }
 
+// Cadastro de Produto
+
 function produtoExiste($nome){
     
     include "config.php";
@@ -66,22 +86,37 @@ function produtoExiste($nome){
     return $total;
 }
 
+function tratar_arquivo_upload($string, $nomeProduto){
+    // pegando a extensao do arquivo
+    $partes 	= explode(".", $string);
+    $extensao 	= $partes[count($partes)-1];	
+    // somente o nome do arquivo
+    $nome	= preg_replace('/\.[^.]*$/', '', $nomeProduto);	
+    // removendo simbolos, acentos etc
+    $a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýýþÿŔŕ?';
+    $b = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuuyybyRr-';
+    $nome = strtr($nome, utf8_decode($a), $b);
+    $nome = str_replace(".","-",$nome);
+    $nome = preg_replace( "/[^0-9a-zA-Z\.]+/",'-',$nome);
+    return utf8_decode(strtolower($nome.".".$extensao));
+}
+
 function cadastrarProduto($dados, $upload){
     include "config.php";
 
     $nome = $dados['nome'];
-    $tamanho = $dados['tamanho'];
-    $categoria = $dados['categoria'];
-    $status = $dados['status'];
+    $categoria_id = $dados['categoria'];
     $valor = $dados['valor'];
     $descricao = $dados['descricao'];
-    $dir = "../_imgBanco/";
+    $status = $dados['status'];
+    $dir = "../_imgBanco";
     $img = $_FILES["picture-input"];
+    $img["name"] = tratar_arquivo_upload(utf8_decode($img['name']), utf8_decode($nome));
     $imgNome = $img["name"];
 
-
     if (produtoExiste($nome) == 0 && move_uploaded_file($img["tmp_name"], "$dir/".$imgNome)) {
-        $sql = "INSERT INTO `produto` (`id`, `nome_produto`, `data_cadastro`, `tamanho`, `categoria`, `valor`, `descricao`, `img`, `status`) VALUES (NULL, '$nome', current_timestamp(), '$tamanho', '$categoria', '$valor', '$descricao', '$dir/$$imgNome', '$status')";
+
+        $sql = "INSERT INTO `produto` (`id`, `nome_produto`, `categoria_id`, `valor_unitario`, `descricao`, `img`, `status`) VALUES (NULL, '$nome', '$categoria_id', '$valor', '$descricao', '$dir/$imgNome', '$status')";
 
         $query = $mysqli->query($sql) or die("ERRO: " . $mysqli->error);
         return $query;
@@ -89,6 +124,53 @@ function cadastrarProduto($dados, $upload){
         return False;
     }
 }
+
+// Cadastro de Categoria
+function categoriaExiste($nome)
+{
+    include "config.php";
+    $sql = "SELECT categoria FROM categoria WHERE categoria='$nome'";
+    $query = $mysqli->query($sql);
+    $total = mysqli_num_rows($query);
+
+    return $total;
+}
+
+function cadastrarCategoria($dados, $upload)
+{
+    include "config.php";
+
+    $nome = $dados['categoria'];
+    $dir = "../_imgBanco";
+    $img = $_FILES["picture-input"];
+    $img["name"] = tratar_arquivo_upload(utf8_decode($img['name']), utf8_decode($nome));
+    $imgNome = $img["name"];
+
+    if (usuarioExiste($cpf) == 0) {
+
+        $sql = "INSERT INTO `categoria` (`id`, `categoria`, `imagem`) VALUES (NULL, '$nome','$dir/$imgNome')";
+
+        $query = $mysqli->query($sql) or die("ERRO: " . $mysqli->error);
+        return $query;
+    } else {
+        return False;
+    }
+}
+
+function editarCategoria($dados, $upload)
+{
+    include "config.php";
+    $id = $dados['id'];
+    $nome = $dados['nomeProduto'];
+    $url = $dados['link'];
+
+    $sql = "UPDATE categoria SET  nome_produto='$nome', fornecedor='$fornecedor', custo_produto='$custo', valor_venda='$valor', estoque_qtd='$qtd', codigo_produto='$codigo', url_img='$url' WHERE id='$id'";
+
+    $query = $mysqli->query($sql) or die("ERRO: " . $mysqli->error);
+    return $query;
+}
+
+// Cadastro de Usuario
 
 function usuarioExiste($cpf)
 {
