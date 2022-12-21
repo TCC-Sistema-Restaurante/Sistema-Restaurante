02955c6ce6cdaa06fd44f77b9bafa59012dc9833
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2022-12-21 15:49:26 -0300

    Back end terminar pedidos pendentes

--- pedidos pendentes/exibirPedidos.php
+++ pedidos pendentes/exibirPedidos.php
@@ -42,44 +42,13 @@ if (isset($_POST['mostrar'])) {
 
 
     $mostrar .=
-      '<button class="mt-1 btnPreparar" id="' . $ids_mesa['id_mesa'] . '" data-bs-toggle="modal" data-bs-target="#staticBackdrop' . $ids_mesa['id_mesa'] . '">Preparar</button>
+      '<button class="mt-1 btnPreparar" id="btnPreparar" onclick="entregar('. $ids_mesa["id_mesa"] .')" data-bs-toggle="modal" data-bs-target="#modal" data-bs-whatevermesaID="' . $ids_mesa["id_mesa"]  .'" data-bs-whateverHora="'. $hora .'" data-bs-whateverProdutos="'. $produtos .'" >Preparar</button>
 
 
             </div>
           </div>
         </div>
-        
-        <div class="modal t-modal fade" id="staticBackdrop' . $ids_mesa['id_mesa'] . '" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg">
-                  <div class="modal-content PrepararModal">
-                      <div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                    <div class="boxPreparar">
-                    <div class="row">
-                      <div class="col-4 text-center parteEsquerdaPreparar">
-                        <p>MESA</p>
-                        <p>' . $ids_mesa['id_mesa'] . '</p>
-
-                        <i class="fa-regular fa-clock"></i>
-                        <p>'.$hora.'</p>
-                      </div>
-
-                      <div class="col-8 parteDireitaPreparar">
-                        <h4 class="">Detalhes do Pedido</h4>
-                        <p> ' . $produtos . '</p>
-                        
-                        
-                        <button id="' . $ids_mesa['id_mesa'] . '" class="mt-1 btnEntregarPreparar">Entregar</button>
-                        
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-              </div>';
+        ';
 
   }
 
